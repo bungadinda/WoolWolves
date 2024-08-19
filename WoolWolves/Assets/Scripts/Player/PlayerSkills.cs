788a384bce43eebbb2307e8e5d382fc62cdecca0
Rusted Canon <hafazah15kano@gmail.com>
2024-08-19 18:18:12 +0700

    Update PlayerSkills.cs

--- WoolWolves/Assets/Scripts/Player/PlayerSkills.cs
+++ WoolWolves/Assets/Scripts/Player/PlayerSkills.cs
@@ -1,35 +1,45 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerSkills : MonoBehaviour
 {
-    protected static bool isWolf = false;
+    protected static bool isWolf = true;  // Default to Wolf
     [SerializeField] private GameObject wolfPrefab;
     [SerializeField] private GameObject sheepPrefab;
-    
+    [SerializeField] private float transformDuration = 5f;  // Durasi transformasi
+    [SerializeField] private float cooldownDuration = 10f;  // Durasi cooldown
+    private bool isOnCooldown = false;
 
     // Update is called once per frame
     public virtual void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Alpha3)) Transforming();
+        if (Input.GetKeyDown(KeyCode.Alpha3) && !isOnCooldown)
+        {
+            StartCoroutine(Transforming());
+        }
     }
-    
-    public void Transforming()
+
+    private IEnumerator Transforming()
     {
-        isWolf = !isWolf;
-        if(isWolf)
-        {
-            wolfPrefab.SetActive(true);
-            sheepPrefab.SetActive(false);
-            Debug.Log("Menjadi Wolf");
-        }
-        else
-        {
-            
-            wolfPrefab.SetActive(false);
-            sheepPrefab.SetActive(true);
-            Debug.Log("Menjadi mbe");
-        }
-    } 
+        // Mulai transformasi menjadi domba
+        isWolf = false;
+        wolfPrefab.SetActive(false);
+        sheepPrefab.SetActive(true);
+        Debug.Log("Menjadi mbe");
+
+        // Menunggu durasi transformasi
+        yield return new WaitForSeconds(transformDuration);
+
+        // Kembali menjadi serigala
+        isWolf = true;
+        wolfPrefab.SetActive(true);
+        sheepPrefab.SetActive(false);
+        Debug.Log("Kembali menjadi Wolf");
+
+        // Mulai cooldown
+        isOnCooldown = true;
+        yield return new WaitForSeconds(cooldownDuration);
+        isOnCooldown = false;
+        Debug.Log("Skill is colldown!");
+    }
 }
