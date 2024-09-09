994c3c2199ff6dcba450b5d474a06ef47c011f1f
Akmaluddin <akmaluddin717@gmail.com>
2024-09-09 17:37:34 +0700

    Intro sebelum gameplay

--- WoolWolves/Assets/Scripts/Tutorial State.cs
+++ WoolWolves/Assets/Scripts/Tutorial State.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TutorialState : MonoBehaviour
 {
+    public Animator animator;
+    public string animationTrigger = "Intro";
     
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,27 @@ public class TutorialState : MonoBehaviour
 
     public void StartTheGame()
     {
+        // Time.timeScale = 1;
+        // Memulai animasi orang jatuh saat tombol ditekan
+        animator.SetTrigger(animationTrigger);
+        // Debug.Log("ok");
+        // yield return new WaitForSecondsRealtime(14f);
+
+        // Ubah Time.timeScale menjadi 1 untuk memulai game
+        // Time.timeScale = 1;
+        // Mulai Coroutine untuk menunggu hingga animasi selesai
+        StartCoroutine(WaitForAnimationToEnd());
+        // Debug.Log("ok");
+    }
+
+    IEnumerator WaitForAnimationToEnd()
+    {
+        Debug.Log("ok");
+        // Tunggu hingga animasi orang jatuh selesai
+
+        yield return new WaitForSecondsRealtime(12f);
+        // Setelah animasi selesai, ubah timescale menjadi 1 agar game dimulai
         Time.timeScale = 1;
+        Debug.Log("Game Started, TimeScale is now 1");
     }
 }
