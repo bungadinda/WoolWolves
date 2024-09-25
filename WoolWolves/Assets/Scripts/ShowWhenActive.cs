950e3a24279324df4b447d87707e49020ff0678f
Tiarasatya <82933768+Tiarasatya@users.noreply.github.com>
2024-09-25 22:28:02 +0700

    Fix screen that appears when pause

--- WoolWolves/Assets/Scripts/ShowWhenActive.cs
+++ WoolWolves/Assets/Scripts/ShowWhenActive.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class ShowWhenActive : MonoBehaviour
 {
-    public GameObject ObjectA; //GameObjet yang ingin ditampilkan
+    public GameObject ObjectA; //GameObjek yang ingin ditampilkan
     public GameObject ObjectB1;  //GameObject yang dipantau
     public GameObject ObjectB2;
+    public GameObject ObjectB3;
+    public GameObject ObjectB4;
+
 
     // Update is called once per frame
     private void Update()
     {
-        if(ObjectB1.activeSelf || ObjectB2.activeSelf)  //cek apakah GameObject B aktif
+        if (ObjectB1.activeSelf || ObjectB2.activeSelf || ObjectB3.activeSelf || ObjectB4.activeSelf)  //cek apakah GameObject B aktif
         {
             ObjectA.SetActive(false);
         }
@@ -19,8 +22,5 @@ public class ShowWhenActive : MonoBehaviour
         {
             ObjectA.SetActive(true);
         }
-
-        
-
     }
 }
